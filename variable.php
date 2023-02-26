4642efbc0484779ab5fb81c149460e60d3e820b2
alfanridho <muha22092ti@nurulfikri.ac.id>
2023-02-27 06:18:30 +0700

    dasar dasar php

---
+++ variable.php
@@ -0,0 +1,15 @@
+<?php
+$nama = 'Data Diri Mahasiswa';
+// jika nama variabel nya sama,tetapi memiliki nilai yang berbeda
+//  maka yang dicetak adalah variable yang kedua
+echo $nama;
+// ================================================
+// definisikan variabel data diri
+$namaSaya = "Muhammad Alfan Ridho";
+$umur = 18;
+$berat = 50;
+
+echo '<br/>Nama : '.$namaSaya;
+echo "<br/>Umur saya adalah $umur Tahun";
+echo "<br/>Berat saya $berat KG";
+?>
