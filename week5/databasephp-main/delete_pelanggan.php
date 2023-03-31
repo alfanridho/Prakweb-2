c6b4c89d30b66bf49840d052aec636b27526abb5
alfanridho <muha22092ti@nurulfikri.ac.id>
2023-03-31 18:54:49 +0700

    week5

---
+++ week5/databasephp-main/delete_pelanggan.php
@@ -0,0 +1,17 @@
+<?php 
+require_once 'dbkoneksi.php';
+?>
+            
+<?php
+    if(isset($_GET['iddel'])){
+        $id = $_GET['iddel'];
+
+        // Query SQL untuk menghapus data produk dengan id yang sesuai
+        $sql = "DELETE FROM pelanggan WHERE id=$id";
+        $result = $dbh->exec($sql);
+
+        // Redirect ke halaman utama setelah proses delete selesai
+        header("Location: list_pelanggan.php");
+        exit();
+    }
+?>
