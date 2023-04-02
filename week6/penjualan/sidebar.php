5bd3ec53f9ac3fb64d054a083c46632fcb3ceaab
alfanridho <muha22092ti@nurulfikri.ac.id>
2023-04-02 08:57:16 +0700

    week6

--- week6/penjualan/sidebar.php
+++ week6/penjualan/sidebar.php
@@ -14,7 +14,7 @@
           <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Muhammad Farhan</a>
+          <a href="#" class="d-block">Muhammad Alfan Ridho</a>
         </div>
       </div>
 
