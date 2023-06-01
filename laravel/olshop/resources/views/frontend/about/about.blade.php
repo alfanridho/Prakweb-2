16e37e51c7942b3164c5f569dd8578e104379a7e
alfanridho <muha22092ti@nurulfikri.ac.id>
2023-06-01 07:25:36 +0700

    templating layout laravel

---
+++ laravel/olshop/resources/views/frontend/about/about.blade.php
@@ -0,0 +1,14 @@
+@extends('frontend.layout.app')
+
+@section('content')
+    <div class="container">
+    <h1 style="text-align: center">Data Diri</h1>
+        <ul>
+            <li>Nama: Muhammad Alfan Ridho</li>
+            <li>NIM: 0110222092</li>
+            <li>TTL: Depok, 08 Juli 2004</li>
+            <li>Alamat: Palsigunung Tugu Cimanggis Depok</li>
+        </ul>
+    </div>
+
+@endsection
--- laravel/olshop/routes/web.php
+++ laravel/olshop/routes/web.php
@@ -3,6 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\InputController;
 use App\Http\Controllers\FormInputController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ProdukController;
+use App\Http\Controllers\FrontendController;
+use App\Http\Controllers\AboutController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -45,3 +49,15 @@ Route::post('output', [InputController::class, 'output']);
 
 Route::get('/form', [FormInputController::class, 'index']);
 Route::post('/form', [FormInputController::class, 'form']);
+
+// ini route tampilan admin
+Route::prefix('admin')->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index']);
+    Route::get('/produk', [ProdukController::class, 'index']);
+});
+
+// Route untuk tampilan frontend
+Route::prefix('frontend')->group(function () {
+    Route::get('/dashboard', [FrontendController::class, 'index']);
+    Route::get('/about', [AboutController::class, 'index']);
+});
